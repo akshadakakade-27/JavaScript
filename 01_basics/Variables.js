6ffce47915eda19d50151b48c6ddf739c7a02f0e
akshadakakade-27 <153642374+akshadakakade-27@users.noreply.github.com>
2025-05-05 16:16:57 +0000

    variable file

---
+++ 01_basics/Variables.js
@@ -0,0 +1,21 @@
+const accountId=1444553;
+let accountemail="akshadakakade@gmail,com"
+var accountPassword ="12345"
+accountCity="pune"
+
+// accountId=90
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountemail);
+console.log(accountCity);
+
+
+/*
+not use var caue of issue of functional issue
+*/
+
+// updating variables 
+accountPassword="555555"
+accountemail="abc@gmail.com"
+accountCity="mumbai"
+console.table([accountId,accountPassword,accountemail,accountCity]); // to print in table format
