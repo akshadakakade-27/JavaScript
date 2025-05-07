300d91fa600d3527b724467a9f7880c5b398d0d0
akshadakakade-27 <153642374+akshadakakade-27@users.noreply.github.com>
2025-05-07 18:15:38 +0000

    new file

---
+++ 01_basics/data_types.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+//code readablity should high
+
+//number ==> 2 to power 53 (2^53)
+//bigint
+//string =""
+//null ==> standalone value (empty)
+//undefined =>
+//symbol=> unique
+
+// object
+console.log(typeof "Akshada")
+console.log(typeof 53);
+console.log(typeof undefined)  // undefined
+console.log(typeof null); // object
