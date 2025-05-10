7710010aca405e1c922cb449cfa473788c553053
AKSHADA KAKADE <153642374+akshadakakade-27@users.noreply.github.com>
2025-05-10 15:56:30 +0000

    converion file

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let age =35
+console.log(typeof age);
+let score ="35abc"
+console.log(typeof score)
+let valueInnum= Number(age)
+console.log( typeof  valueInnum)
+console.log(valueInnum)
+let valin = Number(score)
+console.log( score)
+console.log(valin)
+/*
+Number(....)
+"33"- 33
+"33abc" - NaN
+true - 1; false - 0
+null - 0
+ Boolean ( .... )
+ true = 1 false = 0
+ ""= fallse "akshu"=true
+
+ String(....)
+*/
+let st=String(age)
+console.log(st )
+console.log( typeof st)
